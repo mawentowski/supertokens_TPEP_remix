9084e0a9dc37e21b31751a8a3423f812206f5ba3
Mark Wentowski <mawentowski@Marks-MacBook-Pro.local>
2024-02-05 10:06:33 -0600

    initial app setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,17 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 
 export default function App() {
   return (
-    <html lang="en">
+    <html>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
       </head>
       <body>
+        <h1>Hello world!</h1>
         <Outlet />
-        <ScrollRestoration />
+
         <Scripts />
         <LiveReload />
       </body>
---
+++ notes.md
@@ -0,0 +1,7 @@
+```javascript
+//  start the server (express)
+npm run dev
+// The client/server entries points are:
+entry.client.tsx
+entry.server.tsx
+```
