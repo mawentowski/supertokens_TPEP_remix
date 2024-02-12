7ff71957579f46b315d513e9785362c49e0aeb98
Mark Wentowski <mawentowski@Marks-MacBook-Pro.local>
2024-02-12 15:52:27 -0600

    changed app name

--- app/config/appInfo.tsx
+++ app/config/appInfo.tsx
@@ -1,6 +1,6 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-  appName: "supertokens_nextjs_client",
+  appName: "supertokens_remix_client",
   apiDomain: "http://localhost:3000",
   websiteDomain: "http://localhost:3000",
   apiBasePath: "/api/auth",
